50293591dc850f75dc855d641f7ac2fd18b90f20
Hasan A. Tekeoğlu <55619655+tekeoglan@users.noreply.github.com>
2025-07-08 19:35:46 +0300

    fix: broken paths

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,28 +1,15 @@
-{ pkgs, ... }:
-
+{ pkgs, config, ... }:
 {
-  # Example: Install a package from the default (stable) channel
-  home.packages = [
-    pkgs.neovim
-  ];
-
-  # Example: Install a package specifically from the unstable channel
-  # Let's say 'my-new-app' is only available or is a newer version in unstable
-  # home.packages = home.packages ++ [ # Append to existing packages list
-  #   unstablePkgs.my-new-app # <--- Installing 'my-new-app' from the unstable channel
-  #   unstablePkgs.another-unstable-tool
-  # ];
-
-  # Example: If you want to use an unstable version of a package that's also in stable
-  # e.g., a newer version of 'cowsay'
-  # home.packages = home.packages ++ [
-  #   unstablePkgs.cowsay # This will get the unstable cowsay
-  # ];
-
-  # Alternatively, for program configurations:
-  # programs.some-program = {
-  #   enable = true;
-  #   package = unstablePkgs.some-program; # Force it to use the unstable version
-  #   # ... other options
-  # };
+  home.packages = with pkgs; let
+    graphicalApps = [
+      zotero
+    ];
+    wrappedApps = map config.lib.nixGL.wrap graphicalApps;
+  in
+    [
+      neovim
+      zellij
+      lazygit
+      ncspot
+    ] ++ wrappedApps;
 }
